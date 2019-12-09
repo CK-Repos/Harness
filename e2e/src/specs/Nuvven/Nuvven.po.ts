040279b7f52f4981962ffa497d8029d5602055d0
CK-Repos <58254245+CK-Repos@users.noreply.github.com>
2019-12-09 14:31:26 +0530

    Update Nuvven.po.ts

--- e2e/src/specs/Nuvven/Nuvven.po.ts
+++ e2e/src/specs/Nuvven/Nuvven.po.ts
@@ -16,4 +16,19 @@ export class NuvvenPO {
     readonly ResetButton = ['xpath', "//div[@id='screen_393772834']/div/a"];
     readonly StartSystem = ['xpath', "//div[@id='screen_393772835']/div/a"];
 
+    // Booking
+
+    readonly BookingMousehover = ['xpath', "//a[contains(text(),'1052590969')]"];
+    readonly Bookings = ['xpath', "//div[@id='screen_396015795']/div/a"];
+    readonly createnew = ['xpath', "//a[contains(text(),'1046792897')]"];
+        readonly Search = ['xpath', "//a[contains(text(),'1046793375')]"];
+        readonly cartype = ['xpath', "//a[contains(text(),'1053639566')]"];
+        readonly cartypeclose = ['xpath', "//a[contains(text(),'1053639580')]"];
+        readonly searchfilter = ['xpath', "//a[contains(text(),'1107979234')]"];
+        readonly vehicleselection = ['xpath', "//a[contains(text(),'1107980178')]"];
+        readonly RmSelection = ['xpath', "//a[contains(text(),'1046800654')]"];
+        readonly ProceedButton = ['xpath', "//a[contains(text(),'1046802397')]"];
+        readonly CreateQuoteButton = ['xpath', "//a[contains(text(),'1108413359')]"];
+        readonly PayConfirmbutton = ['xpath', "//a[contains(text(),'1108414435')]"];
+        readonly paynowbutton = ['xpath', "//a[contains(text(),'1054595902')]"];
 }
